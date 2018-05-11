ce81a92ad4b24ff0f612f7ce3dae9a524ebbabf9
Julio Gonzalez Altamirano <devjga@gmail.com>
2018-05-11 10:25:54 -0500

    Prepare initial function problem.

---
+++ inventory.py
@@ -0,0 +1,13 @@
+
+# This is in all caps because it is a "global" variable in this python module
+PARKING_SPACE_COUNTS = [10, 50, 30]
+
+def add_capacity(parking_space_counts):
+    # take each member of the parking_space_counts list and add one space to that member. Use a loop!
+    # print out the resulting number after you add the space for each member
+    # print out that the function is done. I included this line for you below.
+    print('Done.')
+
+# This will call the add_spaces function. From the terminal command line, call "python inventory.py" to load and run this module
+add_capacity(PARKING_SPACE_COUNTS)
+
