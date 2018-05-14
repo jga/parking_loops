9de127b4309f3b2750c96436a545632db2e5b905
Julio Gonzalez Altamirano <devjga@gmail.com>
2018-05-14 07:21:03 -1000

    Edit Visco's solution.

--- inventory.py
+++ inventory.py
@@ -1,11 +1,14 @@
 
 PARKING_SPACE_COUNTS = [10, 50, 30]
 
+
 def add_capacity(parking_space_counts):
-    for ints in PARKING_SPACE_COUNTS:
-        print(ints + 1)
+    # changing name from non-descript ints to specific descriptor
+    for parking_space_count in PARKING_SPACE_COUNTS:
+        print(parking_space_count + 1)
+    # Moved back into the function because instructions were to note the *function" was done, not the module
+    print('Done.')
 
 
 add_capacity(PARKING_SPACE_COUNTS)
 
-print('Done.')
